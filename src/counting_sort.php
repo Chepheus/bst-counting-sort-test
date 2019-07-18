4af7cfd29cd885d7547244dde198b3a2708751b1
anton-zamniborshch <anton.zamniborshch@boosta.co>
2019-07-18 10:46:39 +0300

    Change rand int for decrease performance

--- src/counting_sort.php
+++ src/counting_sort.php
@@ -2,9 +2,9 @@
 
 include_once 'CountingSort/counting_sort.php';
 
-$testList = array();
-foreach (range(1, 1000) as $key) {
-    $testList[] = random_int(1, 2);
+$testList = [];
+foreach (range(1, 2) as $key) {
+    $testList[] = random_int(1, 100000);
 }
 $testList = countingSort($testList);
 print_r($testList);
